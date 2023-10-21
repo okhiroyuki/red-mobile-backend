b7a32f683731c0e37fe7d8a09e390669f51f9f7d
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2023-10-21 11:23:22 +0900

    feat: remove grunt

--- scripts/android/gruntRelease.js
+++
@@ -1,6 +0,0 @@
-module.exports = function () {
-  const execSync = require("child_process").execSync;
-  console.log("grunt release");
-  const result = execSync("npx grunt release").toString();
-  console.log(result);
-};
