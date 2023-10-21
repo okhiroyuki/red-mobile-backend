db418a1e3dec46e06d94f3983f2f77cab13e3ba0
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2023-10-21 11:23:03 +0900

    feat: add new script

---
+++ scripts/android/copyWWW.js
@@ -0,0 +1,38 @@
+module.exports = function () {
+  const fs = require("fs-extra");
+  const path = require("path");
+
+  async function clean() {
+    const currentWorkingDirectory = process.cwd();
+    const pathsToClean = [
+      "www/assets/",
+      "www/css/",
+      "www/js",
+      "www/img/",
+      "www/*.html",
+      "www/*.icon",
+    ];
+
+    for (const p of pathsToClean) {
+      await fs.remove(path.resolve(currentWorkingDirectory, p));
+    }
+  }
+
+  async function copy() {
+    const currentWorkingDirectory = process.cwd();
+    const source = path.resolve(currentWorkingDirectory, "dist/");
+    const destination = path.resolve(currentWorkingDirectory, "www/");
+
+    await fs.copy(source, destination);
+  }
+
+  async function build() {
+    await clean();
+    await copy();
+  }
+
+  build().catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+};
